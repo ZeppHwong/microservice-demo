17d0bb0448170d6a3d2bef455370f721531fcd5b
ZeppHwong <kimheejoong@163.com>
2023-06-15 10:30:13 +0800

    add springcloud gateway

---
+++ springcloud-demo/gateway-sample/src/main/java/cn/demo/gateway/limit/BucketLimit.java
@@ -0,0 +1,20 @@
+package cn.demo.gateway.limit;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class BucketLimit {
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                return Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+            }
+        };
+    }
+}

d100518f705f9516e9e8a66c3cfb9d94f7ba0160
ZeppHwong <kimheejoong@163.com>
2023-09-01 16:46:00 +0800

    add k8s config map

--- springcloud-demo/k8s-sample/src/main/java/cn/demo/k8s/controller/Hello.java
+++ springcloud-demo/k8s-sample/src/main/java/cn/demo/k8s/controller/Hello.java
@@ -1,13 +1,30 @@
 package cn.demo.k8s.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
+    @Value("${greeting.message:default}")
+    private String greeting;
 
-    @RequestMapping("/hello")
+    @Value("${farewell.message:default}")
+    private String farewell;
+
+    @GetMapping("/hello")
     public String hello(){
         return "hello k8s";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return greeting;
+    }
+
+    @GetMapping("/farewell")
+    public String farewell(){
+        return farewell;
     }
 }
